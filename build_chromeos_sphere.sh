c4e40b0712d65c4949c758c2f7570fe34545310f
Martin Krastev <blu.dark@gmail.com>
2020-08-22 14:53:24 +0300

    refactor build scripts

--- build_chromeos_sphere.sh
+++ build_chromeos_sphere.sh
@@ -13,7 +13,7 @@ SOURCES=(
 	util_file.cpp
 	util_misc.cpp
 )
-CFLAGS=(
+CXXFLAGS=(
 	-fno-exceptions
 	-fno-rtti
 	-fstrict-aliasing
@@ -38,17 +38,19 @@ LFLAGS=(
 	-lGLESv2
 )
 
+source cxx_util.sh
+
 if [[ ${HOSTTYPE:0:3} == "arm" ]]; then
 
 	# some distro vendors insist on targeting Thumb2 on ARM - go proper ARM
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-marm
 	)
 
 	ASIMD=`cat /proc/cpuinfo | grep "^Features" | grep -m1 -o -e neon -e asimd`
 
 	if [[ -n $ASIMD ]]; then
-		CFLAGS+=(
+		CXXFLAGS+=(
 			-mfpu=neon
 		)
 	fi
@@ -56,65 +58,16 @@ if [[ ${HOSTTYPE:0:3} == "arm" ]]; then
 	# check for RK3399 based on bigLITTLE config
 	if [ `cat /proc/cpuinfo | grep "^CPU part[[:space:]]*: 0xd08" | wc -l` -eq 2 -a \
 	     `cat /proc/cpuinfo | grep "^CPU part[[:space:]]*: 0xd03" | wc -l` -eq 4 ]; then
-		CFLAGS+=(
+		CXXFLAGS+=(
 			-DQUIRK0001_SYSTEM_CRASH_AT_EXIT
 		)
 	fi
 
-	# clang may fail auto-detecting the host armv7/armv8 cpu on some setups; collect all part numbers
-	UARCH=`cat /proc/cpuinfo | grep "^CPU part" | sed s/^[^[:digit:]]*//`
-
-	# in order of preference, in case of big.LITTLE (armv7 and armv8 lumped together)
-	if   [ `echo $UARCH | grep -c 0xd09` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv8-a
-			-mtune=cortex-a73
-		)
-	elif [ `echo $UARCH | grep -c 0xd08` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv8-a
-			-mtune=cortex-a72
-		)
-	elif [ `echo $UARCH | grep -c 0xd07` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv8-a
-			-mtune=cortex-a57
-		)
-	elif [ `echo $UARCH | grep -c 0xd03` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv8-a
-			-mtune=cortex-a53
-		)
-	elif [ `echo $UARCH | grep -c 0xc0f` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv7-a
-			-mtune=cortex-a15
-		)
-	elif [ `echo $UARCH | grep -c 0xc0e` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv7-a
-			-mtune=cortex-a17
-		)
-	elif [ `echo $UARCH | grep -c 0xc09` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv7-a
-			-mtune=cortex-a9
-		)
-	elif [ `echo $UARCH | grep -c 0xc08` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv7-a
-			-mtune=cortex-a8
-		)
-	elif [ `echo $UARCH | grep -c 0xc07` -ne 0 ]; then
-		CFLAGS+=(
-			-march=armv7-a
-			-mtune=cortex-a7
-		)
-	fi
+	cxx_uarch_arm
 
 elif [[ ${HOSTTYPE:0:3} == "x86" ]]; then
 
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-march=native
 		-mtune=native
 	)
@@ -122,14 +75,14 @@ elif [[ ${HOSTTYPE:0:3} == "x86" ]]; then
 fi
 
 if [[ $1 == "debug" ]]; then
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-Wall
 		-O0
 		-g
 		-DDEBUG
 	)
 else
-	CFLAGS+=(
+	CXXFLAGS+=(
 		-ffast-math
 		-funroll-loops
 		-O3
@@ -137,6 +90,6 @@ else
 	)
 fi
 
-BUILD_CMD="./clang++.sh -o "${TARGET}" -pipe "${CFLAGS[@]}" -x c++ "${SOURCES[@]}" -x c "${SOURCES_C[@]}" "${LFLAGS[@]}
+BUILD_CMD="./clang++.sh -o "${TARGET}" -pipe "${CXXFLAGS[@]}" -x c++ "${SOURCES[@]}" -x c "${SOURCES_C[@]}" "${LFLAGS[@]}
 echo $BUILD_CMD
 $BUILD_CMD
